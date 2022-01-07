ef139ae73f59621570a7327cf072fcbd906101a0
Smlcrp <smlcrp@gmail.com>
2022-01-07 17:19:10 -0500

    Added kings but broken king movement

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let cells = document.querySelectorAll("td");
 // let whiteCells = document.querySelectorAll(".whiteSquare");
 let greenCells = document.querySelectorAll(".greenSquare");
 let pieces = document.querySelectorAll("span");
+let kingPiece = document.querySelectorAll(".kingPiece")
 let playerPieces = document.querySelectorAll(".playerPieces");
 let opponentPieces = document.querySelectorAll(".opponentPieces");
 let resetButton = document.querySelector(".resetButton");
@@ -12,23 +13,39 @@ let turn = "red";
 let moveFinished = true;
 
 
+
 let currentIndex;
 
 
 // this is my click event that allows you to select/deselect/move game pieces on your specific turn
-
+console.log(rows)
+console.log(rows[0].childNodes[3])
 cells.forEach((cell, index) => {
     cell.addEventListener("click", function() {
-        if(cell.classList[1] === "playerPiece" && turn === "red" && moveFinished) {
+        if(ifKingPlayer(index) && turn === "red" && moveFinished) {
+            cell.classList.remove("kingPiece");
+            cell.classList.remove("playerPiece");
+            moveFinished = false;
+            currentIndex = index;
+        }else if(ifKingPlayer(index) && turn === "red" && !moveFinished) {
+            if(validKingMovePlayer(currentIndex, index)) {
+                cell.classList.add("playerPiece");
+                cell.classList.add("kingPiece");
+            }
+        }else if(cell.classList[1] === "playerPiece" && turn === "red" && moveFinished) {
             cell.classList.remove('playerPiece')
             moveFinished = false;
             console.log(cell.classList)
             console.log(index)
             currentIndex = index;
         }else if(cell.classList[0] === "greenSquare" && turn === "red" && !moveFinished) {
-            console.log(cells[index].classList)
-            console.log(currentIndex)
-            if(isValidPlayerMove(currentIndex, index)) {
+            // console.log(cells[index].classList)
+            // console.log(currentIndex)
+            if(becomeKingPlayer(index)) {
+                moveFinished = true;
+                cell.classList.add("kingPiece");
+                turn = "black";
+            }else if(isValidPlayerMove(currentIndex, index)) {
                 cell.classList.add("playerPiece")
                 moveFinished = true;
                 turn = "black";
@@ -56,10 +73,14 @@ cells.forEach((cell, index) => {
             console.log(index)
             currentIndex = index;
         }else if(cell.classList[0] === "greenSquare" && turn === "black" && !moveFinished) {
-            console.log(cells[index].classList)
-            console.log(currentIndex)
-            if(isValidOpponentMove(currentIndex, index)) {
-            cell.classList.add("opponentPiece") 
+            if(becomeKingOpponent(index)) {
+                moveFinished = true;
+                cell.classList.add("kingPiece");
+                cell.classList[2].innerHTML = "O";
+                turn = "red";
+            }else if(isValidOpponentMove(currentIndex, index)) {
+                console.log(index)
+                cell.classList.add("opponentPiece") 
                 moveFinished = true;
                 turn = "red";
             }else if(isValidOpponentJump(currentIndex, index)){
@@ -80,8 +101,7 @@ cells.forEach((cell, index) => {
         }
     })
 })
-        
-        
+
            //this function limits the spaces the red game pieces can move to to the green spaces diagonally in front of it 
 function isValidPlayerMove(currentIndex, index) {
     if((currentIndex - index === 7 && cells[index].classList.value !== "greenSquare playerPiece") || (currentIndex - index === 9 && cells[index].classList.value !== "greenSquare playerPiece")) {
@@ -124,3 +144,63 @@ function isValidOpponentJump(currentIndex, index) {
         return true;
     }
 }
+
+function ifKingPlayer(index) {
+    if(cells[index].classList.value === "greenSquare kingPiece") {
+        console.log(true);
+        return true;
+    }
+    console.log(cells[index].classList.value);
+    return false;
+}
+function ifKingOpponent(index) {
+    if(cells[index].classList.value === "greenSquare opponentPiece kingPiece") {
+        return true;
+    }
+    return false;
+}
+
+function becomeKingPlayer(index) {
+    if(index === 1 || index === 3 || index === 5 || index === 7) {
+        console.log(true);
+        return true;
+    }
+    console.log(false);
+    return false;
+}
+function becomeKingOpponent(index) {
+    if(index === 56 || index === 58 || index === 60 || index === 62) {
+        console.log(true);
+        return true;
+    }
+    console.log(false);
+    return false;
+}
+
+function validKingMovePlayer(currentIndex, index) {
+    if((currentIndex - index === 7 && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === 9 && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === -7 && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === -9 && cells[index].classList[2].value == "kingPiece")) {
+        console.log(true);
+        return true;
+    }
+    console.log(false)
+    return false;
+}
+
+function validKingMoveOpponent(currentIndex, index) {
+    if((currentIndex - index === 7 && cells[index].classList.value !== "greenSquare opponentPiece" && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === 9 && cells[index].classList.value !== "greenSquare opponentPiece" && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === -7 && cells[index].classList.value !== "greenSquare opponentPiece" && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === -9 && cells[index].classList.value !== "greenSquare opponentPiece" && cells[index].classList[2].value == "kingPiece")) {
+        console.log(true);
+        return true;
+    }
+    console.log(false)
+    return false;
+}
+
+function validKingJump(currentIndex, index) {
+    if((currentIndex - index === 14 && cells[index].classList.value === "greenSquare" && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === 18 && cells[index].classList.value === "greenSquare" && cells[index].classList[2].value == "kingPiece") || (currentIndex - index === -14 && cells[index].classList.value === "greenSquare" && sKing) || (currentIndex - index === -18 && cells[index].classList.value === "greenSquare" && sKing)) {
+        console.log(true)
+        return true;
+    }
+    console.log(false)
+    return false;
+}
+
